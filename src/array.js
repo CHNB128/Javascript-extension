9adbf30e5f2849f1ce6ab08348174b64ad23bc62
drake.aren <drake.aren@protonmail.com>
2019-02-02 08:40:25 +0600

    add some array function

---
+++ src/array.js
@@ -0,0 +1,16 @@
+/**
+ * @description zip two array
+ * @param array -> Array
+ * @usage [1,2,3].zip([2,3,4]) -> [[1,2], [2,3], [3,4]]
+ */
+
+Array.prototype.zip = function(array) {
+  return this.map((element, index) => [element, array[index]])
+}
+
+/**
+ * @description return first item form array
+ */
+Array.prototype.first = function() {
+  return this[0]
+}
